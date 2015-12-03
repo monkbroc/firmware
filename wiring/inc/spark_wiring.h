24087b928afd6c239c4f6eafbb20aa7079cc8f0a
Julien Vanier <jvanier@gmail.com>
2015-12-03 15:21:36 -0500

    Keep the 2 analogWrite prototypes

--- wiring/inc/spark_wiring.h
+++ wiring/inc/spark_wiring.h
@@ -67,7 +67,8 @@ PinMode getPinMode(uint16_t pin);
 bool pinAvailable(uint16_t pin);
 void digitalWrite(uint16_t pin, uint8_t value);
 int32_t digitalRead(uint16_t pin);
-void analogWrite(uint16_t pin, uint16_t value, uint16_t pwm_frequency = TIM_PWM_FREQ);
+void analogWrite(uint16_t pin, uint16_t value);
+void analogWrite(uint16_t pin, uint16_t value, uint16_t pwm_frequency);
 
 
 long map(long value, long fromStart, long fromEnd, long toStart, long toEnd);
